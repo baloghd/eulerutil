c90192492ad7dcbc494515b3608e11247f20537f
baloghd <balogh.domonkos@gmail.com>
2019-01-04 11:22:49 +0100

    isprime

--- eulerutil/prime.py
+++ eulerutil/prime.py
@@ -1,11 +1,16 @@
+from typing import Iterable
+
+def is_multiple_of_any(n: int, numbers: Iterable) -> bool:
+    for num in numbers:
+        if n % num == 0:
+            return True
+    return False
+
 def is_prime(n: int) -> bool:
-    """return True if n is prime"""
-    if n < 2:
-        return False
-    
     for i in range(2, n):
+        if i*i > n:
+            break
         if n % i == 0:
             return False
-        
     return True
             
