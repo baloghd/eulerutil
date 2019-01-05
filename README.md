2244d14c12689eb4891ec4f4ff1dd1e0c36a86af
baloghd <balogh.domonkos@gmail.com>
2019-01-05 19:11:45 +0100

    Readme update

--- README.md
+++ README.md
@@ -9,7 +9,17 @@ A Python library of utility functions used for solving Project Euler problems
 
 
 ### number_theory
+
+*even, odd*: just for convenience, I didn't adopt the NodeJS way ;) 
+
 *gcd*: Euclid's GCD 
+ 
+*numdivisors, numdigits, sumdigits*
+
+*divides*
+
+*Collatz_sequence*
+
 
 ### prime
 *is_prime*: prime testing 
--- eulerutil/number_theory.py
+++ eulerutil/number_theory.py
@@ -1,4 +1,10 @@
-from typing import Tuple, Set
+from typing import Tuple, Set, List
+
+def even(n: int) -> bool:
+    return n % 2 == 0
+
+def odd(n: int) -> bool:
+    return not even(n)
 
 def gcd(a: int, b: int) -> int:
     while b > 0:
@@ -35,3 +41,18 @@ def sumdigits(n: int) -> int:
         d += n % 10
         n //= 10
     return d
+
+def divides(a: int, b: int) -> bool:
+    return b % a == 0
+
+def Collatz_sequence(n: int) -> List[int]:
+    seq = []
+    t = n
+    while n != 1:
+        seq.append(str(int(n)))
+        if even(n):
+            n /= 2
+        else:
+            n = 3*n + 1
+    seq.append(1)
+    return dict(zip(seq, [i for i in range(len(seq) - 1, 0, -1)]))
